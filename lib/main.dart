1a63e25328c2155a55f30affe89f6b5e1fc5a2cc
Aayush014 <aayushpatel1411@gmail.com>
2023-08-04 18:00:15 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: const Color(0xffEEEDED),
+        appBar: AppBar(
+          backgroundColor: const Color(0xffEF6262),
+          title: const Text("My RNW"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: RichText(
+            text: const TextSpan(
+              children: [
+                TextSpan(
+                  text: ("Red & White\n"),
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontSize: 60,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: ("\t\t\t\t\t\t Multimidea Education\n"),
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: ('\t\tShaping"skills" for"scaling higher"...!!'),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.red,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
